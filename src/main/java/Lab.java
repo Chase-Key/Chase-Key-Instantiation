75f57ca9d20759127c9e8d52ce2d1d4610454b27
Chase Key <Chase.d.key@gmail.com>
2023-02-20 16:36:03 +0000

    edited line 21-22 in instatiateANewObject()

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        String accountName = new String();
+        return accountName;
     }
 }
